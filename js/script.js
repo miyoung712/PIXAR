baccb8a95b485896b5b9a5f47ce4531901966ebc
miyoung712 <93234918+miyoung712@users.noreply.github.com>
2022-01-12 15:21:00 +0900

    add

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+let offset = 0;
+
+window.addEventListener('wheel', e => {
+  offset += Math.sign(e.deltaY) * 60;
+
+  if (offset < 0) {
+    offset = 0;
+  } else if (offset > 10000 - window.innerWidth) {
+    offset = 10000 - window.innerWidth;
+  }
+
+  container.style.transform = `translateX(-${offset}px`;
+
+  console.log(offset);
+  document.getElementById('position').innerHTML=offset;  //offset값 표시
+  
+});
+
+
